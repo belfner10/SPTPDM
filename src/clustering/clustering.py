395253d3d4f624d0570912f308b6591e46601c5a
belfner10 <elfnerbm@mail.uc.edu>
2022-03-02 22:55:15 -0500

    Updated clustering code

--- src/clustering/clustering.py
+++ src/clustering/clustering.py
@@ -1,21 +1,86 @@
 import cv2
 import numpy as np
 from scipy.cluster.hierarchy import linkage
+from sklearn.cluster import KMeans
 from skimage.measure import label
 
-from src.clustering.funcs import create_clusters
+from src.clustering.funcs import create_n_clusters
 from src.segmenter.funcs import simplify_image
 
+from random import sample
+from collections import defaultdict
+import matplotlib.pyplot as plt
 
-def create_several_clustes(vecs, image_path):
-    Z = linkage(vecs, 'complete')
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    return image
+# colors are from the muted color set at https://personal.sron.nl/~pault/
+# colors are in RGB format
+colors = [[204, 102, 119], [51, 34, 136], [221, 204, 119], [17, 119, 51], [136, 204, 238], [136, 34, 85], [68, 170, 153], [153, 153, 51], [170, 68, 153], [221, 221, 221], [0, 0, 0]]
 
 
+def create_color_maps(n_clusters):
+    # TODO add check to make sure n_clusters is less than or equal to colors somewhere upstream
+    selected_colors = sample(colors, n_clusters)
+    rmap = {}
+    gmap = {}
+    bmap = {}
+
+    for x in range(n_clusters):
+        rmap[x] = selected_colors[x][0]
+        gmap[x] = selected_colors[x][1]
+        bmap[x] = selected_colors[x][2]
+
+    r_l = lambda x: rmap[x]
+    g_l = lambda x: gmap[x]
+    b_l = lambda x: bmap[x]
+
+    return r_l, g_l, b_l
 
 
-if __name__ == '__main__':
+def invert_dict(d):
+    ret = defaultdict(list)
+    for key, value in d.items():
+        ret[value].append(key)
+    return ret
+
+
+def hierarchical_clustering(comps, labeled, n_clusters, method='ward'):
+    Z = linkage(comps, method)
+    clusters = create_n_clusters(Z, len(comps), num_clusters=n_clusters)
+    labeled_clusters = np.vectorize(lambda x: clusters[x])(labeled)
+
+    # inv = invert_dict(clusters)
+    #
+    # for key, value in inv.items():
+    #     print(len(value))
+
+    return labeled_clusters
+
+
+def k_means_clustering(comps, labeled, n_clusters):
+    kmeans = KMeans(n_clusters).fit(comps)
+    clusters = {}
+    for x, label in enumerate(kmeans.labels_):
+        clusters[x + 1] = label
+    labeled_clusters = np.vectorize(lambda c: clusters[c])(labeled)
+    return labeled_clusters
+
+
+def draw_clusters(comps, labeled, filename, n_clusters=5, method='kmeans'):
+    if method == 'kmeans':
+        labeled_clusters = k_means_clustering(comps, labeled, n_clusters)
+    elif method == 'hierarchical':
+        labeled_clusters = hierarchical_clustering(comps, labeled, n_clusters)
+    else:
+        raise ValueError(f'Unknown method name: {method}. Possible values are [\'kmeans\', \'hierarchical\']')
+
+    r_l, g_l, b_l = create_color_maps(n_clusters)
+    img_r = np.vectorize(r_l)(labeled_clusters)
+    img_g = np.vectorize(g_l)(labeled_clusters)
+    img_b = np.vectorize(b_l)(labeled_clusters)
+    img = np.stack((img_r, img_g, img_b), axis=2).astype(np.uint8)
+    cv2.imwrite(filename, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+
+
+def main():
     vecs = np.load('out_grarep.np.npy')[:, 1:]
 
     Z = linkage(vecs, 'complete')
@@ -26,10 +91,21 @@ if __name__ == '__main__':
     print('Creating Images')
     for r in range(2, 10):
         print(f'Creating Image {r}')
-        m = create_clusters(Z, len(vecs), r)
+        m = create_n_clusters(Z, len(vecs), r)
         mult = int(250 / r)
         out = np.zeros(labeled.shape, dtype='uint8')
         for y in range(labeled.shape[0]):
             for x in range(labeled.shape[1]):
                 out[y, x] = m[labeled[y, x]] * mult
         cv2.imwrite(f'out{r}.png', out)
+
+
+def main2():
+    comps = np.load('comps.npy')
+    labeled = np.load('labeled.npy')
+    print(comps.shape)
+    draw_clusters(comps, labeled, n_clusters=10)
+
+
+if __name__ == '__main__':
+    main2()
--- src/clustering/funcs.py
+++ src/clustering/funcs.py
@@ -1,37 +1,50 @@
-import numpy as np
-from sklearn.linear_model import LinearRegression
-
-
-def l_method(x, y):
-    out = np.zeros(len(x))
-    for i in range(2, len(x) - 1):
-        reg = LinearRegression().fit(x[:i], y[:i])
-        score1 = reg.score(x[:i], y[:i])
-        # print(score1)
-        reg = LinearRegression().fit(x[i:], y[i:])
-        score2 = reg.score(x[i:], y[i:])
-        # print(score2)
-        # print(score1+score2)
-        out[i] = score1 + score2
-
-    return np.argmax(out)
-
 def get_all_sub(cluster_id, Z, num_rows):
+    """
+    Combines all sub clusters
+    Parameters
+    ----------
+    cluster_id :
+    Z :
+        linkage matrix
+    num_rows :
+
+    Returns
+    -------
+    list: all data ids in the cluster
+    """
     if cluster_id < num_rows:
         return [cluster_id]
     else:
-        return get_all_sub(int(Z[cluster_id - num_rows][0]), Z, num_rows) + get_all_sub(int(Z[cluster_id - num_rows][1]), Z, num_rows)
+        left_id = round(Z[cluster_id - num_rows][0])
+        right_id = round(Z[cluster_id - num_rows][1])
+        return get_all_sub(left_id, Z, num_rows) + get_all_sub(right_id, Z, num_rows)
 
-def create_clusters(Z, num_rows, num_clusters=2):
+
+# TODO does num_rows really need to be specified
+def create_n_clusters(Z, num_rows, num_clusters=2):
+    """
+    creates the top 'num_clusters' clusters
+    Parameters
+    ----------
+    Z :
+        linkage matrix
+    num_rows :
+    num_clusters :
+
+    Returns
+    -------
+
+    """
     cluster_seeds = [len(Z) + num_rows - 1]
+    # incrementaly splits highest cluster on dendrogram until the desired number of clusters are found
     while len(cluster_seeds) < num_clusters:
         cid = max(cluster_seeds)
         cluster_seeds.remove(cid)
-        cluster_seeds.append(int(Z[cid - num_rows][0]))
-        cluster_seeds.append(int(Z[cid - num_rows][1]))
+        cluster_seeds.append(round(Z[cid - num_rows][0]))
+        cluster_seeds.append(round(Z[cid - num_rows][1]))
 
     ret = {}
-    for x,cid in enumerate(cluster_seeds):
+    for x, cid in enumerate(cluster_seeds):
         for id in get_all_sub(cid, Z, num_rows):
-            ret[id+1] = x
+            ret[id + 1] = x
     return ret
