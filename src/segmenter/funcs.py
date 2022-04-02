89d9568edd5ab92cdb5a4cba0b4cf95232dad43a
belfner10 <elfnerbm@mail.uc.edu>
2022-04-02 14:21:00 -0400

    Improved simplification methods

--- src/segmenter/funcs.py
+++ src/segmenter/funcs.py
@@ -4,10 +4,54 @@ from typing import Callable
 import cv2
 import numpy as np
 import scipy.sparse as sp
-from skimage.measure import label
+from skimage.measure import label, regionprops
 
 
-def window(image: np.ndarray, y: int, x: int, value: int, method: Callable) -> (bool, int):
+def get_neighbor_coords(arr, coord_set):
+    neighbors = set()
+    height, width = arr.shape
+    for coord in coord_set:
+        y, x = coord
+        yl = y - 1 if y != 0 else y
+        yu = y + 2 if y != height - 1 else y + 1
+        xl = x - 1 if x != 0 else x
+        xu = x + 2 if x != width - 1 else x + 1
+        for yp in range(yl, yu):
+            for xp in range(xl, xu):
+                if (yp, xp) not in coord_set:
+                    neighbors.add((yp, xp))
+    return neighbors
+
+
+def get_coord_labels(arr, coords):
+    labels = []
+    for coord in coords:
+        y, x = coord
+        labels.append(arr[y, x])
+    return labels
+
+
+def get_surrounding(arr, coords):
+    coord_set = set([(c[0], c[1]) for c in coords])
+    neighbors = get_neighbor_coords(arr, coord_set)
+    labels = get_coord_labels(arr, neighbors)
+    c = Counter(labels)
+    return c.most_common(1)[0][0]
+
+
+def adv_simplify(arr, threshold=4):
+    ret = arr.copy()
+    labeled, num_regions = label(arr, return_num=True)
+    props = regionprops(labeled)
+    for x, prop in enumerate(props):
+        if prop['area'] <= threshold:
+            l = get_surrounding(arr, prop['coords'])
+            for coord in prop['coords']:
+                y, x = coord[0], coord[1]
+                ret[y, x] = l
+    return ret
+
+def window(image: np.ndarray, y: int, x: int, value: int) -> (bool, list):
     """
     Analyzed the surrounding pixels to check if any adjacent pixels share the same class
     If none exist, choose a new pixel class based on the surrounding pixels
@@ -21,8 +65,6 @@ def window(image: np.ndarray, y: int, x: int, value: int, method: Callable) -> (
         x index of the pixel to be analyzed
     value: int
         class of the pixel to be analyzed TODO: this argument could be removed, time might suffer
-    method: Callable
-        method of selecting the new class of the pixel
 
     Returns
     -------
@@ -42,7 +84,42 @@ def window(image: np.ndarray, y: int, x: int, value: int, method: Callable) -> (
                 if value == tv:
                     return True, 0
                 neighbors.append(tv)
-    return False, method(neighbors)
+    return False, neighbors
+
+
+def window2(image: np.ndarray, y: int, x: int, value: int) -> list:
+    """
+    Analyzed the surrounding pixels to check if any adjacent pixels share the same class
+    If none exist, choose a new pixel class based on the surrounding pixels
+    Parameters
+    ----------
+    image: NDArray
+        the passed image
+    y: int
+        y index of the pixel to be analyzed
+    x: int
+        x index of the pixel to be analyzed
+    value: int
+        class of the pixel to be analyzed TODO: this argument could be removed, time might suffer
+
+    Returns
+    -------
+
+    """
+    height, width = image.shape
+    neighbors = []
+    yl = y - 1 if y != 0 else y
+    yu = y + 2 if y != height - 1 else y + 1
+    xl = x - 1 if x != 0 else x
+    xu = x + 2 if x != width - 1 else x + 1
+
+    for yp in range(yl, yu):
+        for xp in range(xl, xu):
+            if yp != y or xp != x:
+                tv = image[yp, xp]
+                if value != tv:
+                    neighbors.append(tv)
+    return neighbors
 
 
 def simplify_image(image: np.ndarray, method: str) -> np.ndarray:
@@ -78,7 +155,7 @@ def simplify_image(image: np.ndarray, method: str) -> np.ndarray:
                 r = 1
             value = image[y, x]
 
-            near, new_value = window(image, y, x, value, selected_method)
+            near, new_value = selected_method(window(image, y, x, value))
             if near:
                 simplified_image[y, x] = value
             else:
@@ -87,7 +164,7 @@ def simplify_image(image: np.ndarray, method: str) -> np.ndarray:
     return simplified_image
 
 
-def create_dict_adj(image: np.ndarray, labeled_image: np.ndarray, norm_weights: bool = True) -> (dict, defaultdict):
+def create_dict_adj(image: np.ndarray, labeled_image: np.ndarray, norm_weights: bool = True, ignore_labels=None) -> (dict, defaultdict):
     """
     Creates adjacency matrix in the form of dictionary
     Parameters
@@ -111,15 +188,17 @@ def create_dict_adj(image: np.ndarray, labeled_image: np.ndarray, norm_weights: 
     for x in range(width):
         for y in range(height):
             region_label = labeled_image[y, x]
+            if region_label in ignore_labels:
+                continue
             if region_label not in label_classes:
                 label_classes[region_label] = image[y, x]
-            if y != 0:
+            if y != 0 and labeled_image[y - 1, x] != region_label:
                 weights[region_label][labeled_image[y - 1, x]] += 1
-            if x != 0:
+            if x != 0 and labeled_image[y, x - 1] != region_label:
                 weights[region_label][labeled_image[y, x - 1]] += 1
-            if y != height - 1:
+            if y != height - 1 and labeled_image[y + 1, x] != region_label:
                 weights[region_label][labeled_image[y + 1, x]] += 1
-            if x != width - 1:
+            if x != width - 1 and labeled_image[y, x + 1] != region_label:
                 weights[region_label][labeled_image[y, x + 1]] += 1
 
     if not norm_weights:
@@ -133,8 +212,7 @@ def create_dict_adj(image: np.ndarray, labeled_image: np.ndarray, norm_weights: 
 
     for key, value in weights.items():
         for k2, v2 in value.items():
-            if key < k2:
-                value[k2] = v2 / max_val
+            value[k2] = v2 / max_val
 
     return label_classes, weights
 
@@ -235,7 +313,7 @@ def dict_to_sp(d, num_labels):
     for key, value in d.items():
         for k2, v2 in value.items():
             if key != k2:
-                weights[key-1, k2-1] = v2
+                weights[key - 1, k2 - 1] = v2
     return sp.csr_matrix(weights)
 
 
@@ -256,8 +334,9 @@ def draw_regions(image, regions: np.ndarray) -> np.ndarray:
                     cv2.rectangle(drawing_image, ((x + 1) * scale_factor - 1 - thickness, y * scale_factor - 1), ((x + 1) * scale_factor - 1 + thickness, y * scale_factor + scale_factor - 1), 255, -1)
     return drawing_image
 
+
 # TODO rename np to sp
-def create_np_adj_from_image(image: np.array, smp_method: str = None, verbose=False):
+def create_np_adj_from_image(image: np.array, smp_method: str = None, verbose=False, max_size=None):
     if smp_method:
         image = simplify_image(image, smp_method)
     labeled, num_regions = label(image, return_num=True)
@@ -265,11 +344,19 @@ def create_np_adj_from_image(image: np.array, smp_method: str = None, verbose=Fa
     if verbose:
         print(f'Number of regions: {num_regions}')
 
-    label_classes, weights = create_dict_adj(image, labeled, num_regions)
+    if max_size:
+        props = regionprops(labeled)
+        ignore_labels = set([prop['label'] for prop in props if prop['area'] > max_size])
+    else:
+        ignore_labels = set()
+
+    label_classes, weights = create_dict_adj(image, labeled, True, ignore_labels)
+    # print(weights)
     weights = dict_to_sp(weights, num_regions)
 
     # label_classes, weights = create_sp_adj(image, labeled, num_regions)
-    return label_classes, weights, labeled
+    return label_classes, weights, labeled, ignore_labels
+
 
 def create_dict_adj_from_image(image: np.array, smp_method: str = None, verbose=False):
     if smp_method:
@@ -282,6 +369,7 @@ def create_dict_adj_from_image(image: np.array, smp_method: str = None, verbose=
     label_classes, weights = create_dict_adj(image, labeled, num_regions)
 
     return label_classes, weights, labeled
+
 
 if __name__ == '__main__':
     simplify_image(1, 'max')
