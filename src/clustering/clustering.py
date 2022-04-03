ab7c8392261da1807bf0dfae3f84c9036247268c
belfner10 <elfnerbm@mail.uc.edu>
2022-04-03 15:34:16 -0400

    Updated clustering code

--- src/clustering/clustering.py
+++ src/clustering/clustering.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 from scipy.cluster.hierarchy import linkage
-from sklearn.cluster import KMeans
+from scipy.spatial import distance_matrix
+from sklearn.cluster import KMeans, SpectralClustering
 from skimage.measure import label
 
 from src.clustering.funcs import create_n_clusters
@@ -35,6 +36,25 @@ def create_color_maps(n_clusters):
     return r_l, g_l, b_l
 
 
+def create_color_maps2(seeds):
+    # TODO add check to make sure n_clusters is less than or equal to colors somewhere upstream
+    selected_colors = [colors[x] for x, _ in seeds]
+    rmap = {}
+    gmap = {}
+    bmap = {}
+
+    for x in range(len(seeds)):
+        rmap[x] = selected_colors[x][0]
+        gmap[x] = selected_colors[x][1]
+        bmap[x] = selected_colors[x][2]
+
+    r_l = lambda x: rmap[x]
+    g_l = lambda x: gmap[x]
+    b_l = lambda x: bmap[x]
+
+    return r_l, g_l, b_l
+
+
 def invert_dict(d):
     ret = defaultdict(list)
     for key, value in d.items():
@@ -42,17 +62,19 @@ def invert_dict(d):
     return ret
 
 
-def hierarchical_clustering(comps, labeled, n_clusters, method='ward'):
+def hierarchical_clustering(comps, labeled, n_clusters, method='ward', ignored=set()):
     Z = linkage(comps, method)
-    clusters = create_n_clusters(Z, len(comps), num_clusters=n_clusters)
+    clusters, seeds = create_n_clusters(Z, len(comps), num_clusters=n_clusters)
     labeled_clusters = np.vectorize(lambda x: clusters[x])(labeled)
 
-    # inv = invert_dict(clusters)
-    #
-    # for key, value in inv.items():
-    #     print(len(value))
+    # for label in ignored:
 
-    return labeled_clusters
+    inv = invert_dict(clusters)
+
+    for key, value in inv.items():
+        print(len(value))
+
+    return labeled_clusters, seeds
 
 
 def k_means_clustering(comps, labeled, n_clusters):
@@ -60,19 +82,83 @@ def k_means_clustering(comps, labeled, n_clusters):
     clusters = {}
     for x, label in enumerate(kmeans.labels_):
         clusters[x + 1] = label
+
+    inv = invert_dict(clusters)
+
+    for key, value in inv.items():
+        print(len(value))
+
     labeled_clusters = np.vectorize(lambda c: clusters[c])(labeled)
     return labeled_clusters
 
 
-def draw_clusters(comps, labeled, filename, n_clusters=5, method='kmeans'):
+def bi_k_means_clustering(comps, labeled, n_clusters):
+    bi_comps = np.copy(comps)
+    current_clusters = 1
+    clusters = {}
+    xs = np.array(range(len(comps)))
+    temp_clusters = []
+    while current_clusters != n_clusters:
+        kmeans = KMeans(n_clusters=2).fit(bi_comps)
+
+        cluster_centers = kmeans.cluster_centers_
+        sse = [0] * 2
+        for point, label in zip(bi_comps, kmeans.labels_):
+            sse[label] += np.square(point - cluster_centers[label]).sum()
+        chosen_cluster = np.argmax(sse, axis=0)
+        if chosen_cluster == 0:
+            temp_clusters.append((sse[0], np.array([pt for pt, label in zip(xs, kmeans.labels_) if label == 0])))
+            temp_clusters.append((sse[1], np.array([pt for pt, label in zip(xs, kmeans.labels_) if label != 0])))
+        else:
+            temp_clusters.append((sse[1], np.array([pt for pt, label in zip(xs, kmeans.labels_) if label == 1])))
+            temp_clusters.append((sse[0], np.array([pt for pt, label in zip(xs, kmeans.labels_) if label != 1])))
+
+        if current_clusters == n_clusters - 1:
+            break
+
+        temp_clusters.sort(key=lambda x: x[0])
+
+        bi_comps = comps[temp_clusters[-1][1]]
+        xs = temp_clusters[-1][1]
+        temp_clusters.pop(-1)
+        current_clusters += 1
+    x = 0
+    for _, pts in temp_clusters:
+        for pt in pts:
+            clusters[pt + 1] = x
+        x += 1
+
+    inv = invert_dict(clusters)
+
+    for key, value in inv.items():
+        print(len(value))
+
+    labeled_clusters = np.vectorize(lambda c: clusters[c])(labeled)
+    return labeled_clusters
+
+
+def spectral_clustering(comps, labeled, n_clusters):
+    spectral = SpectralClustering(n_clusters=n_clusters).fit(comps)
+    clusters = {}
+    for x, label in enumerate(spectral.labels_):
+        clusters[x + 1] = label
+    labeled_clusters = np.vectorize(lambda c: clusters[c])(labeled)
+    return labeled_clusters
+
+
+def draw_clusters(comps, labeled, filename, n_clusters=5, method='kmeans', ignored=set()):
     if method == 'kmeans':
-        labeled_clusters = k_means_clustering(comps, labeled, n_clusters)
+        labeled_clusters = bi_k_means_clustering(comps, labeled, n_clusters)
+        r_l, g_l, b_l = create_color_maps(n_clusters)
     elif method == 'hierarchical':
-        labeled_clusters = hierarchical_clustering(comps, labeled, n_clusters)
+        labeled_clusters, seeds = hierarchical_clustering(comps, labeled, n_clusters)
+        r_l, g_l, b_l = create_color_maps2(seeds)
+    elif method == 'spectral':
+        labeled_clusters = spectral_clustering(comps, labeled, n_clusters)
+        r_l, g_l, b_l = create_color_maps(n_clusters)
     else:
         raise ValueError(f'Unknown method name: {method}. Possible values are [\'kmeans\', \'hierarchical\']')
 
-    r_l, g_l, b_l = create_color_maps(n_clusters)
     img_r = np.vectorize(r_l)(labeled_clusters)
     img_g = np.vectorize(g_l)(labeled_clusters)
     img_b = np.vectorize(b_l)(labeled_clusters)
@@ -82,9 +168,9 @@ def draw_clusters(comps, labeled, filename, n_clusters=5, method='kmeans'):
 
 def draw_labels(labeled, n_labels, filename):
     r_l, g_l, b_l = create_color_maps(n_labels)
-    img_r = np.vectorize(r_l)(labeled-1)
-    img_g = np.vectorize(g_l)(labeled-1)
-    img_b = np.vectorize(b_l)(labeled-1)
+    img_r = np.vectorize(r_l)(labeled - 1)
+    img_g = np.vectorize(g_l)(labeled - 1)
+    img_b = np.vectorize(b_l)(labeled - 1)
     img = np.stack((img_r, img_g, img_b), axis=2).astype(np.uint8)
     cv2.imwrite(filename, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
--- src/clustering/funcs.py
+++ src/clustering/funcs.py
@@ -35,16 +35,18 @@ def create_n_clusters(Z, num_rows, num_clusters=2):
     -------
 
     """
-    cluster_seeds = [len(Z) + num_rows - 1]
+    avalible_col = list(range(1, num_clusters))
+    cluster_seeds = [(0, len(Z) + num_rows - 1)]
     # incrementaly splits highest cluster on dendrogram until the desired number of clusters are found
     while len(cluster_seeds) < num_clusters:
-        cid = max(cluster_seeds)
+        cid = max(cluster_seeds,key=lambda x:x[1])
         cluster_seeds.remove(cid)
-        cluster_seeds.append(round(Z[cid - num_rows][0]))
-        cluster_seeds.append(round(Z[cid - num_rows][1]))
+        cluster_seeds.append((cid[0], round(Z[cid[1] - num_rows][0])))
+        cluster_seeds.append((avalible_col[0],round(Z[cid[1] - num_rows][1])))
+        avalible_col.remove(avalible_col[0])
 
     ret = {}
     for x, cid in enumerate(cluster_seeds):
-        for id in get_all_sub(cid, Z, num_rows):
+        for id in get_all_sub(cid[1], Z, num_rows):
             ret[id + 1] = x
-    return ret
+    return ret, cluster_seeds
